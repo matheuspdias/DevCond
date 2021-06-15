cbf5c02a4fbed7e8d2cb6db1aa321de946a73fdd
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-06-14 21:39:19 -0300

    Seeder com dados inicial

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,63 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('units')->insert([
+            'name' => 'APT 100',
+            'id_owner' => '1'
+        ]);
+
+        DB::table('units')->insert([
+            'name' => 'APT 101',
+            'id_owner' => '1'
+        ]);
+
+        DB::table('units')->insert([
+            'name' => 'APT 200',
+            'id_owner' => '0'
+        ]);
+
+        DB::table('units')->insert([
+            'name' => 'APT 201',
+            'id_owner' => '0'
+        ]);
+
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Academia',
+            'cover' => 'gym.jpg',
+            'days' => '1,2,4,5',
+            'start_time' => '06:00:00',
+            'end_time' => '22:00:00'
+        ]);
+
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Piscina',
+            'cover' => 'pool.jpg',
+            'days' => '1,2,3,4,5',
+            'start_time' => '07:00:00',
+            'end_time' => '23:00:00'
+        ]);
+
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Churrasqueira',
+            'cover' => 'barbecue.jpg',
+            'days' => '4,5,6',
+            'start_time' => '06:00:00',
+            'end_time' => '22:00:00'
+        ]);
+
+        DB::table('walls')->insert([
+            'title' => 'Título de aviso de teste',
+            'body' => 'lorem impsum taltal',
+            'datecreated' => '2020-12+20 15:00:00'
+        ]);
+
+        DB::table('walls')->insert([
+            'title' => 'Alerta geral para todos',
+            'body' => 'Atenção no alerta',
+            'datecreated' => '2020-12+20 18:00:00'
+        ]);
     }
 }
